b376175a821cd153a64e1ad4abfbe611449c8492
valentimkoniarski <valentim.koniarski@hotmail.com>
2022-03-06 19:54:48 -0300

    cors

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,17 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-    baseURL: 'https://car-ads.herokuapp.com/',
-    responseType: 'json'
+  baseURL: "https://car-ads.herokuapp.com/",
+  responseType: "json",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "X-Requested-With, content-type, Authorization",
+  },
 });
 
-api.interceptors.request.use(async config => {
-    const token = getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+api.interceptors.request.use(async (config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
